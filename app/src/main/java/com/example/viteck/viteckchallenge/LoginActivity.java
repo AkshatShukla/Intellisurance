66fffa77bbd86e7bfc6ff14f7840d4db07b243b7
EGJ <egj100812@gmail.com>
2017-12-02 05:30:50 -0500

    Fixed Bugs

--- app/src/main/java/com/example/viteck/viteckchallenge/LoginActivity.java
+++ app/src/main/java/com/example/viteck/viteckchallenge/LoginActivity.java
@@ -7,14 +7,19 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.ldoublem.loadingviewlib.view.LVBlock;
@@ -92,6 +97,7 @@ public class LoginActivity extends AppCompatActivity {
         mPasswordField = (EditText) findViewById(R.id.passwordField);
         mLoginBtn = (Button) findViewById(R.id.loginBtn);
         mSignupBtn = (TextView) findViewById(R.id.signupBtn);
+        forgotPass = findViewById(R.id.forgotPass);
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -113,6 +119,9 @@ public class LoginActivity extends AppCompatActivity {
                             // Start your app main activity
                             mLVBlock.stopAnim();
                             // close this activity
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
 
                             //mProgress.setMessage("Give Us A Moment...");
                             //mProgress.show();
@@ -126,7 +135,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //startSignIn();
+                startSignIn();
 
             }
         });
@@ -149,5 +158,69 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(LoginActivity.this);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void startSignIn() {
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            //User did not enter any email or password.
+            Snackbar snackbar = Snackbar
+                    .make(li, R.string.empty_field, Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+        else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Snackbar snackbar = Snackbar
+                    .make(li, R.string.incorrect_email, Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+        else {
+
+            progressDialog.setMessage("Logging in...");
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (!task.isSuccessful()) {
+                        //if incorrect email/password entered.
+                        progressDialog.dismiss();
+                        Snackbar snackbar = Snackbar
+                                .make(li, R.string.auth_failed, Snackbar.LENGTH_LONG);
+                        snackbar.show();
+                    } else {
+                        Snackbar snackbar = Snackbar
+                                .make(li, R.string.auth_success, Snackbar.LENGTH_LONG);
+                        snackbar.show();
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                }
+            });
+        }
+
+    }
 
 }
